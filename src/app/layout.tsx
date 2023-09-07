4c49339ee189840524e3c2a05c978049343749ea
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-09-07 02:23:14 -0700

    updated default text color for owner cards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html className="bg-[#181c28]" lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
       <Toaster position="bottom-right" />
     </html>
   );
