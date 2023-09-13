3dd439f622e3a8ca04c9b18a29be65c83319e33b
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-09-13 12:55:29 -0700

    fixed icon import issues by dyanmically creating the component inline

--- src/components/HeroFeatures.tsx
+++ src/components/HeroFeatures.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import { Feature, features } from "@/config/siteConfig";
-import Icon from "./Icon";
+import { icons } from "@/config/icons";
 
 export default function HeroFeatures() {
   return (
     <div className="flex space-x-10">
       {features.map((feature: Feature, idx: number) => {
+        const Icon = icons[feature.icon];
+
         return (
           <div key={idx} className="h-[100px] w-[375px] flex space-x-4">
             <div className="h-full relative flex flex-col justify-start">
               <div className="h-[50px] w-[50px] inset-0 absolute flex justify-center items-center rounded-lg bg-blue-500 blur"></div>
               <div className="h-[50px] w-[50px] relative flex justify-center items-center rounded-lg bg-gray-800">
-                <Icon name={feature.icon} size={25} color="#60a5fa" />
+                <Icon size={25} color="#60a5fa" />
               </div>
             </div>
 
