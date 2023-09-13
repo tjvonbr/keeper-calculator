6ad19fa5d976610497ee24605a131de183784ed7
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-09-13 02:21:40 -0700

    no longer default export for icon component

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -6,7 +6,7 @@ export interface IconProps {
   size: number;
 }
 
-export default function Icon({ name, color, size }: IconProps) {
+export function Icon({ name, color, size }: IconProps) {
   const LucideIcon = icons[name];
 
   return <LucideIcon color={color} size={size} />;
