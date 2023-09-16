21f48549261f1aad5a3578670cd0c239493daec9
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-09-15 17:32:13 -0700

    prisma migration for dropped passes

---
+++ src/app/api/crons/fetch-player-dropped-passes.ts
@@ -0,0 +1,65 @@
+import { db } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import puppeteer from "puppeteer";
+
+export async function GET() {
+  try {
+    const url =
+      "https://www.fantasypros.com/nfl/advanced-stats-wr.php?year=2023&range=week&week=1";
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, {
+      timeout: 0,
+    });
+
+    const players = await page.$$eval("tbody > tr", (players) => {
+      return players.map((p) => {
+        const fullName = p.querySelector("a")?.innerText;
+        const team = p.querySelector("small")?.innerText;
+        const droppedPasses = p.querySelector("td:nth-child(19)")?.innerHTML;
+
+        return {
+          fullName,
+          team,
+          droppedPasses,
+        };
+      });
+    });
+
+    await browser.close();
+
+    for (const player of players) {
+      const fullName = player.fullName?.split(" ") as string[];
+      const firstName = fullName[0];
+      const lastName =
+        fullName.length > 2 ? fullName.splice(-2).join(" ") : fullName[1];
+      const teamName = player.team === "JAC" ? "JAX" : player.team;
+
+      const dbPlayer = await db.player.findFirst({
+        where: {
+          firstName,
+          lastName,
+          team: teamName,
+        },
+      });
+
+      if (dbPlayer) {
+        await db.droppedPasses.create({
+          data: {
+            playerId: dbPlayer.id,
+            week: 1,
+            year: 2023,
+            droppedPasses: Number(player.droppedPasses),
+          },
+        });
+      } else {
+        console.log(fullName);
+      }
+    }
+
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
