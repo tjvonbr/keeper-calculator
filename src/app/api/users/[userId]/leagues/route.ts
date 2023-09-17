44bbc701260e001b54278bbd0ecdd51fec6dfeb3
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-09-17 12:29:48 -0700

    recreate draftboard

---
+++ src/app/api/users/[userId]/leagues/route.ts
@@ -0,0 +1,33 @@
+import { db } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const url = "https://api.sleeper.app/v1/players/nfl";
+
+    const response = await fetch(url);
+    const rawPlayers = await response.json();
+
+    const players: any[] = Object.values(rawPlayers);
+
+    for (const player of players) {
+      await db.player.create({
+        data: {
+          firstName: player.first_name,
+          lastName: player.last_name ?? null,
+          height: player.height ?? null,
+          weight: player.weight ?? null,
+          team: player.team ?? null,
+          jerseyNumber: player.number ?? null,
+          position: player.depth_chart_position ?? null,
+          fantasyId: player.fantasy_data_id ?? null,
+          sleeperId: player.player_id,
+        },
+      });
+    }
+
+    return NextResponse.json(200);
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
--- src/app/leagues/[leagueId]/draft/page.tsx
+++ src/app/leagues/[leagueId]/draft/page.tsx
@@ -1,7 +1,81 @@
-export default function LeagueDraft() {
+import { getDraftById, getLeagueById } from "@/lib/helpers";
+import Image from "next/image";
+import { renderPlayerPosition } from "@/lib/utils";
+import { notFound } from "next/navigation";
+
+interface DraftProps {
+  params: {
+    leagueId: string;
+  };
+}
+
+export default async function LeagueDraft({
+  params: { leagueId },
+}: DraftProps) {
+  const league = await getLeagueById(leagueId);
+
+  if (!league) {
+    return notFound();
+  }
+
+  const { draftResults, draft } = await getDraftById(league.draftId);
+  draftResults.sort((playerA, playerB) => playerA.pick_no - playerB.pick_no);
+
   return (
-    <div>
-      <h1>Draft</h1>
+    <div className="min-h-screen flex flex-col">
+      <h1 className="text-2xl text-white font-extrabold">Draft</h1>
+      <div className="mt-56 flex flex-col space-y-2">
+        {draftRows(draft.settings.rounds, draftResults)}
+      </div>
     </div>
   );
+}
+
+function draftRows(rounds: number, draftPicks: any[]) {
+  const draftRounds = Array(rounds).fill(0);
+
+  return draftRounds.map((_: any, idx: number) => {
+    const roundPicks = draftPicks.filter((player) => player.round === idx + 1);
+
+    if ((idx + 1) % 2 === 0) {
+      roundPicks.reverse();
+    }
+
+    return (
+      <div key={idx} className="min-w-screen px-5 flex space-x-2">
+        {roundPicks.map((player: any, idx: number) => {
+          const playerFirstInitial = player.firstName
+            ? player.firstName.split("")[0] + "."
+            : "";
+          return (
+            <div
+              key={idx}
+              className="box-border h-[50px] w-[120px] px-[3px] py-[2px] bg-white rounded-md text-black flex-grow overflow-hidden"
+            >
+              <div className="w-full flex relative">
+                <p className="w-[80%] truncate text-xs text-black font-bold">
+                  {playerFirstInitial} {player.lastName}
+                </p>
+                <p className="w-[20%] text-xs text-gray-600 font-medium">
+                  {player.pick_no}
+                </p>
+              </div>
+              <div className="flex space-x-1 text-xs text-black font-medium">
+                <p>{player.team}</p>
+                <p>{"-"}</p>
+                <p>{renderPlayerPosition(player.position)}</p>
+              </div>
+              <Image
+                src={`https://sleepercdn.com/content/nfl/players/${player.sleeperId}.jpg`}
+                alt={`${player.lastName} Avatar`}
+                className="absolute right-0 bottom-0"
+                height="16"
+                width="22"
+              />
+            </div>
+          );
+        })}
+      </div>
+    );
+  });
 }
